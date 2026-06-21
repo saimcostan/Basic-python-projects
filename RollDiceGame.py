7f4c55aad5b9abcd58bdb01b57d80ea89a56f0b7
Muhammad Saim Rashid <saimcostan1122@gmail.com>
2026-06-21 12:55:08 +0500

    commit

---
+++ RollDiceGame.py
@@ -0,0 +1,17 @@
+import random
+while True:
+    #get input from user
+    choice=input("Roll the dice?(y/n): ")
+    if choice=="y":
+        #if user says yes then generate two random numbers
+        Ans1=str(random.randint(1,6))
+        Ans2=str(random.randint(1,6))
+        print("(",Ans1,",",Ans2,")")
+    elif choice=="n":
+        #end the game if user says no
+        print("thanks for playing")
+        break
+    else:
+        #ignore any other choices
+        print("Invalid choice")
+        
