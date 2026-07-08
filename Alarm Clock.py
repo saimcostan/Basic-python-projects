b2e52c91e3b6e5b55480c1906bf096b4629d64c2
Muhammad Saim Rashid <saimcostan1122@gmail.com>
2026-07-08 15:55:21 +0500

    Update Alarm Clock.py

--- Alarm Clock.py
+++ Alarm Clock.py
@@ -6,13 +6,17 @@ import pygame
 
 
 def Set_Alarm(AlarmTime):
+
+    # printing the alarm time and placing the sound file in a variable
     print("Alarm set for",AlarmTime)
     sound_file="alarm sound.wav"
 
     while True:
+        # printing current time each second until the alarm tiem is reached
         current_time=datetime.datetime.now().strftime("%H:%M:%S")
         print(current_time)
 
+        # playing the sound file using pygame when the alarm time has been reached
         if current_time==AlarmTime:
             print("WAKE UP! 😫")
 
@@ -20,6 +24,7 @@ def Set_Alarm(AlarmTime):
             pygame.mixer.music.load(sound_file)
             pygame.mixer.music.play()
 
+            # keep on repeating the alarm sound
             while pygame.mixer.music.get_busy():
                 time.sleep(1)
 
@@ -27,7 +32,7 @@ def Set_Alarm(AlarmTime):
 
         time.sleep(1)
 
-
+# getting user input about alarm time and calling Set_Alarm
 def main():
     AlarmTime=input("Enter the alarm time (HH:MM:SS): ")
     Set_Alarm(AlarmTime)
